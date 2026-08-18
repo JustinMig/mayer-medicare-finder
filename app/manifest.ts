04098780541eeb25e78fd5b6b71f57aa71533f1f
JustinMig <Justin@mayerig.com>
2026-08-18 09:50:28 -0500

    Use Medicare Finder logo in web app manifest

--- app/manifest.ts
+++ app/manifest.ts
@@ -10,8 +10,8 @@ export default function manifest(): MetadataRoute.Manifest {
     background_color: '#d9e7ef',
     theme_color: '#d9e7ef',
     icons: [
-      { src: '/icon-192.png', sizes: '192x192', type: 'image/png', purpose: 'any' },
-      { src: '/icon.png', sizes: '512x512', type: 'image/png', purpose: 'any' }
+      { src: '/medicare-icon.png?size=192', sizes: '192x192', type: 'image/png', purpose: 'any' },
+      { src: '/medicare-icon.png?size=512', sizes: '512x512', type: 'image/png', purpose: 'any' }
     ]
   }
 }
